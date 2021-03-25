9af47ea29d8354a850caadccd797f352a70c6d21
kshyk <jj.turkiewicz@gmail.com>
2021-03-26 00:25:25 +0100

    fix: reformatted code chore: updated spring & lombok versions chore: gradle upgrade

--- src/test/java/com/kshyk/springme/SpringMeApplicationTests.java
+++ src/test/java/com/kshyk/springme/SpringMeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringMeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
